19f32abdc2b0bb4db403babd53ba3ac32ebcafcf
nacl0312 <757584453@qq.com>
2019-01-31 10:53:35 +0800

    添加动态权限

--- app/src/main/java/com/fire/photoselectortest/MainActivity.java
+++ app/src/main/java/com/fire/photoselectortest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.fire.photoselectortest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fire.photoselector.activity.PhotoSelectorActivity;
 import com.fire.photoselector.models.PhotoSelectorSetting;
@@ -21,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_SELECT_PHOTO = 100;
     private static final String TAG = "MainActivity";
+    private static final int REQUEST_PERMISSION_CODE = 1000;
     private ArrayList<String> result = new ArrayList<>();
     private Button btSelectPhoto;
     private RecyclerView rvList;
     private PhotoRecyclerViewAdapter photoRecyclerViewAdapter;
+    private TextView tvSelectSum;
+    private TextView tvColumnCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +40,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btSelectPhoto = (Button) findViewById(R.id.bt_select_photo);
         rvList = (RecyclerView) findViewById(R.id.rv_list);
-        final TextView tvSelectSum = (TextView) findViewById(R.id.tv_select_sum);
-        final TextView tvColumnCount = (TextView) findViewById(R.id.tv_column_count);
+        tvSelectSum = (TextView) findViewById(R.id.tv_select_sum);
+        tvColumnCount = (TextView) findViewById(R.id.tv_column_count);
         rvList.setLayoutManager(new GridLayoutManager(this, 3));
         photoRecyclerViewAdapter = new PhotoRecyclerViewAdapter(this, result, false);
         rvList.setAdapter(photoRecyclerViewAdapter);
         btSelectPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!TextUtils.isEmpty(tvSelectSum.getText().toString().trim()) && !TextUtils.isEmpty(tvColumnCount.getText().toString().trim())) {
-                    selectPhotos(Integer.parseInt(tvSelectSum.getText().toString().trim()), Integer.parseInt(tvColumnCount.getText().toString().trim()));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_CODE);
+                } else {
+                    if (!TextUtils.isEmpty(tvSelectSum.getText().toString().trim()) && !TextUtils.isEmpty(tvColumnCount.getText().toString().trim())) {
+                        selectPhotos(Integer.parseInt(tvSelectSum.getText().toString().trim()), Integer.parseInt(tvColumnCount.getText().toString().trim()));
+                    }
                 }
             }
         });
+
     }
 
     @Override
@@ -56,6 +69,22 @@ public class MainActivity extends AppCompatActivity {
                     result = data.getStringArrayListExtra(PhotoSelectorSetting.LAST_MODIFIED_LIST);
                     boolean isSelectedFullImage = data.getBooleanExtra(PhotoSelectorSetting.SELECTED_FULL_IMAGE, false);
                     photoRecyclerViewAdapter.setList(result, isSelectedFullImage);
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "未获取权限", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!TextUtils.isEmpty(tvSelectSum.getText().toString().trim()) && !TextUtils.isEmpty(tvColumnCount.getText().toString().trim())) {
+                        selectPhotos(Integer.parseInt(tvSelectSum.getText().toString().trim()), Integer.parseInt(tvColumnCount.getText().toString().trim()));
+                    }
                 }
                 break;
         }
